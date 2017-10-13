2a166424326be74fcb9b58508524461dbe2900aa
Jessica Prieto <jessicaprieto31@gmail.com>
2017-10-12 18:39:29 -0600

    add test cases for the new methods in TweetList

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/TweetListTest.java
@@ -2,6 +2,9 @@ package ca.ualberta.cs.lonelytwitter;
 
 import android.test.ActivityInstrumentationTestCase2;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by Jessica on 2017-10-12.
  */
@@ -39,5 +42,35 @@ public class TweetListTest extends ActivityInstrumentationTestCase2{
         tweets.addTweet(tweet);
         Tweet getTweet = tweets.getTweet(0);
         assertEquals(getTweet.getMessage(), tweet.getMessage());
+    }
+
+    public void testGetTweets() {
+        TweetList tweets = new TweetList();
+        Tweet tweet1 = new NormalTweet("test1");
+        Tweet tweet2 = new NormalTweet("test2");
+        Tweet tweet3 = new NormalTweet("test3");
+        Tweet tweet4 = new NormalTweet("test4");
+        tweets.addTweet(tweet1);
+        tweets.addTweet(tweet2);
+        tweets.addTweet(tweet3);
+        tweets.addTweet(tweet4);
+
+        ArrayList<Tweet> getTweets = tweets.getTweets();
+        assertEquals(getTweets, Arrays.asList(tweet1, tweet2, tweet3, tweet4));
+    }
+
+    public void testGetCount() {
+        TweetList tweets = new TweetList();
+        Tweet tweet1 = new NormalTweet("test1");
+        Tweet tweet2 = new NormalTweet("test2");
+        Tweet tweet3 = new NormalTweet("test3");
+        Tweet tweet4 = new NormalTweet("test4");
+        tweets.addTweet(tweet1);
+        tweets.addTweet(tweet2);
+        tweets.addTweet(tweet3);
+        tweets.addTweet(tweet4);
+
+        int tweetsSize = tweets.getCount();
+        assertEquals(4, tweetsSize);
     }
 }
