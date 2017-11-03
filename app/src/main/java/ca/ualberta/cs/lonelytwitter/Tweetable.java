462dc946c8fd70cf7705a51ce6e48d51ebd14d3a
Jessica Prieto <jessicaprieto31@gmail.com>
2017-11-02 23:52:50 -0600

    Refactored Tweetable interface, main.xml layout, and added a few changes to the string values (changes from refactoring are numbered in the comments)

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -4,8 +4,13 @@ import java.util.Date;
 
 /**
  * Created by romansky on 1/14/16.
+ *
+ * Refactored by: Jessica Prieto
+ *
+ * 5. removed modifiers for interface methods because they don't need it.
+ *    Classes that implements the method will use modifiers.
  */
 public interface Tweetable {
-    public String getMessage();
-    public Date getDate();
+    String getMessage();
+    Date getDate();
 }
